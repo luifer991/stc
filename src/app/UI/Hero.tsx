102b14d3e0b8677ee3fa692448a821b6c79633b4
Jorge Morales <luifer991@gmail.com>
2024-03-07 17:20:03 -0500

    using title component

--- src/app/UI/Hero.tsx
+++ src/app/UI/Hero.tsx
@@ -8,14 +8,13 @@ function Hero() {
     <>
       <div className="bg-[url(/pocketforce.jpeg)] bg-cover bg-center h-[830px] w-full text-white md:bg-[url(/darkpocket.png)]">
         <article className='pl-[240px]'>
-          <Title>
-            <h1 className={`text-left text-[68px] pt-12 pb-12 txt-[#ffffffcc]${bevan.className}`}>
-              Promoviendo el <span className='txt-[#d8c690]'> bienestar</span> y<span className='txt-[#d8c690]'> previniendo</span> la violencia
-            </h1>
-            <p className={`text-left max-w-lg txt-sm pb-[80px] md:pb-20 ${libreBaskerville.className}`}>
-              STC es un programa dedicado a promover soluciones adaptables que ayuden a sus usuarios a prevenir la violencia en cualquier situación.
-            </p>
-          </Title>
+          <Title first='Promoviendo el '
+            spanOne='bienestar '
+            w='y '
+            SpanTwo='previniendo '
+            second='la violencia'
+            third='STC es un programa dedicado a promover soluciones adaptables que ayuden a sus usuarios a prevenir la violencia en cualquier situación.'
+          />
           <div className="pl-8 pt-0 mt-0 w-[700px]">
             <Button classb="l-btn" text="¡Empezar Ya!" />
           </div>
