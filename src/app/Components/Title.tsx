33f153d66324356740b55bb0d4258348428accb3
Jorge Morales <luifer991@gmail.com>
2024-03-07 17:19:02 -0500

    create a title component

--- src/app/Components/Title.tsx
+++ src/app/Components/Title.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { bevan, libreBaskerville } from './fonts'
+import { TitleProps } from '@/lib/definitions'
 
-type TitleProps = {
-  children: React.ReactNode
-}
-
-function Title({ children }: TitleProps): React.JSX.Element {
+function Title({ 
+  first, spanOne, SpanTwo, second, third, w 
+}: TitleProps): React.JSX.Element {
   return (
     <>
       <header className='w-[90%] block items-center justify-center ml-[3rem] md:w-[700px]'>
-        {children}
+        <h1 className={`text-left text-[68px] pt-12 pb-12 txt-[#ffffffcc]${bevan.className}`}>
+          {first} <span className='txt-[#d8c690]'> {spanOne}</span> {w}<span className='txt-[#d8c690]'> {SpanTwo} </span> {second}
+        </h1>
+        <p className={`text-left max-w-lg txt-sm pb-[80px] md:pb-20 ${libreBaskerville.className}`}>
+          {third}
+        </p>
       </header>
     </>
   )
