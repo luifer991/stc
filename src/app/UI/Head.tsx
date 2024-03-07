00b016678a9f2ba48d6f883523bea08701f5aecd
Jorge Morales <luifer991@gmail.com>
2024-03-07 17:19:38 -0500

    using title component

--- src/app/UI/Head.tsx
+++ src/app/UI/Head.tsx
@@ -3,17 +3,14 @@ import { libreBaskerville } from "../Components/fonts"
 
 export function Head(){
     return(
-        <div className="bg-[url(/pocketforce.jpeg)] bg-cover bg-center h-screen w-full text-white md:bg-[url(/darkpocket.png)]">
-            <Title>
-                <h1 className='text-left text-9xl pt-32 pb-[1rem] txt-[#ffffffcc] md:text-8xl'>
-                        Sómos <span className='txt-[#d8c690]'>
-                        STC
-                        </span> 
-                </h1>
-                <p className={`text-left max-w-lg txt-sm pb-[5rem] md:pb-20 ${libreBaskerville.className}`}>
-                    STC es un sistema de defensa personal que se adapta a cada tipo de usuario sin importar su condición física.
-                </p>
-            </Title>
+        <div className="bg-[url(/pocketforce.jpeg)] bg-cover bg-center h-[830px] w-full text-white md:bg-[url(/darkpocket.png)] pl-[240px]">
+            <Title first="Sómos "
+            spanOne="STC"
+            w="y somos tu mejor "
+            SpanTwo="aliado"
+            second="para proteger tu vida"
+            third="STC es un sistema de defensa personal que se adapta a cada tipo de usuario sin importar su condición física."
+            />
         </div>
     )
 }
