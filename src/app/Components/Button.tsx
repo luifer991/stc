60e0949190496d7c924d6b7373d20aa6f98a5299
Jorge Morales <luifer991@gmail.com>
2024-03-07 21:31:23 -0500

    removed button

--- src/app/Components/Button.tsx
+++ src/app/Components/Button.tsx
@@ -2,17 +2,16 @@ import React from 'react';
 import Link from 'next/link';
 import { ButtonProps } from '@/lib/definitions';
 
-
 function Button({ classb, text }: ButtonProps) {
   return (
     <>
-        <Link href="#" 
-            target='_blank' 
-            rel='noreferrer'>
-            <button className={classb}>
-                    {text}
-            </button>
-        </Link>
+      <Link
+        className={classb}
+        href="#"
+        target='_blank'
+        rel='noreferrer'>
+        {text}
+      </Link>
     </>
   )
 }
