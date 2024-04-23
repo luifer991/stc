ad0f273d2dc80c0bff5496341cb4b37313a37ac3
Jorge Morales <luifer991@gmail.com>
2024-04-22 21:31:47 -0500

    make it responsive title

--- src/app/Components/Title.tsx
+++ src/app/Components/Title.tsx
@@ -7,11 +7,11 @@ function Title({
 }: TitleProps): React.JSX.Element {
   return (
     <>
-      <header className='w-[90%] block items-center justify-center ml-[3rem] md:w-[700px]'>
-        <h1 className={`text-left text-[68px] pt-12 pb-12 txt-[#ffffffcc]${bevan.className}`}>
+      <header className='w-[60%] ml-2 block items-center justify-center lg:w-[90%]  lg:ml-[3rem] md:w-[700px]'>
+        <h1 className={`text-left txt-[#ffffffcc]${bevan.className} text-[42px] lg:text-[68px] pt-12 pb-12`}>
           {first} <span className='text-clear-gold'> {spanOne}</span> {w}<span className='text-clear-gold'> {SpanTwo} </span> {second}
         </h1>
-        <p className={`text-left max-w-lg txt-sm pb-[80px] md:pb-20 ${libreBaskerville.className}`}>
+        <p className={`text-left lg:max-w-lg txt-sm pb-[80px] md:pb-20 ${libreBaskerville.className}`}>
           {third}
         </p>
       </header>
